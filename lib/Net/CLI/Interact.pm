830bddf1d28ffbcd0384c1d393aa4f6181ea65d2
Oliver Gorwits <oliver@cpan.org>
2011-03-15 21:18:58 +0000

    documentation for Engine, and set wantarray on last_response

--- lib/Net/CLI/Interact.pm
+++ lib/Net/CLI/Interact.pm
@@ -167,8 +167,9 @@ Execute a single command statement on the connected device, and consume output
 until there is a match with the current I<prompt>. The statement is executed
 verbatim on the device, with a newline appended.
 
-In scalar context the C<last_response> is returned; in list context it is
-returned but split into a list on the I<input record separator> (newline).
+In scalar context the C<last_response> is returned (see below). In list
+context the gathered response is returned, only split into a list on the
+I<input record separator> (newline).
 
 =head2 macro( $name, \@params? )
 
@@ -177,12 +178,15 @@ in the loaded Phrasebook. If the Macro contains commands using C<sprintf>
 format variables then the corresponding total number of C<@params> must be
 passed to the method.
 
-In scalar context the C<last_response> is returned; in list context it is
-returned but split into a list on the I<input record separator> (newline).
+In scalar context the C<last_response> is returned (see below). In list
+context the gathered response is returned, only split into a list on the
+I<input record separator> (newline).
 
 =head2 last_response
 
-Returns the gathered output after the most recent C<cmd> or C<macro>.
+Returns the gathered output after the most recent C<cmd> or C<macro>. In
+scalar context all data is returned. In list context the gathered response is
+returned, only split into a list on the I<input record separator> (newline).
 
 =head2 transport
 
--- lib/Net/CLI/Interact/Role/Engine.pm
+++ lib/Net/CLI/Interact/Role/Engine.pm
@@ -14,7 +14,10 @@ has 'last_actionset' => (
 
 sub last_response {
     my $self = shift;
-    return $self->last_actionset->item_at(-2)->response;
+    my $resp = $self->last_actionset->item_at(-2)->response;
+    return (wantarray
+        ? (split $self->transport->irs, $resp)
+        : $resp);
 }
 
 has 'default_continuation' => (
@@ -83,81 +86,30 @@ sub _execute_actions {
         $self->_prompt($self->last_actionset->last->value);
     }
 
-    return (wantarray
-        ? (split $self->transport->irs, $self->last_response)
-        : $self->last_response);
+    return $self->last_response; # context sensitive
 }
 
 1;
 
-# ABSTRACT: Command statement execution and Prompt management
-
-=head1 SYNOPSIS
-
- $s->set_prompt('user_exec');
- 
- $s->macro('show_int_br');
- my $interfaces = $s->last_response;
- 
- $s->cmd('show ip interfaces brief');
- my $same_as_interfaces = $s->last_response;
- 
- $s->macro('to_priv_exec', 'enable_password');
- # prompt is updated automatically
- 
- $s->macro('show_run');
- my $config = $s->last_response;
+# ABSTRACT: Statement Execution Engine
 
 =head1 DESCRIPTION
 
-This module implements the API for command execution and Prompt management in
-L<Net::CLI::Interact>. After the Phrasebooks are loaded, a table of Prompts
-and a table of Macros is available to the application.
+This module is the core of L<Net::CLI::Interact>, and serves to take entries from your loaded L<Phrasebooks|Net::CLI::Interact::Phrasebook>, issue them to connected devices, and gather the returned output.
 
 =head1 METHODS
 
 =over 4
 
-=item prompt
+=item cmd($command_statement)
 
-Returns the current Prompt, which is a regular expression reference. The
-Prompt is used as a default when a Macro has not been set up with explicit
-Prompt matching.
+Execute a single command statement on the connected device, and consume output
+until there is a match with the current I<prompt>. The statement is executed
+verbatim on the device, with a newline appended.
 
-Typically you would set the prompt you are expecting (see below) once for a
-sequence of commands in a particular CLI context. The C<prompt> is also
-automatically updated when a Macro successfully ends which was defined in the
-Phrasebook with a known Prompt (referenced by name using C<match prompt...>).
-
-=item unset_prompt
-
-You can use this method to empty the current Prompt setting (see above). The
-effect is that the module will automatically set the Prompt for itself based
-on the last line of output received from the connected CLI. Do not use this
-option unless you know what you are doing.
-
-=item set_prompt($prompt_name)
-
-This method will be used most commonly by applications to set a prompt from
-the Phrasebook which matches the current context of the connected CLI session.
-This allows a sequence of commands to be sent which share the same Prompt.
-
-=item find_prompt
-
-A helper method that consumes output from the connected CLI session until a
-line matches one of the named Prompts in the loaded Phrasebooks, at which
-point no more output is consumed. As a consequence the C<prompt> will be set
-(see above) and also the C<last_*> (see below) accessors.
-
-This might be used when you are connecting to a device which maintains CLI
-state between sessions (for example a serial console), and you need to
-discover the current state. However, C<find_prompt> is executed automatically
-for you if you call a C<cmd> or C<macro> before any interaction with the CLI.
-
-You might need to send some input to the device to trigger generation of
-output for matching. If no output matches, the module will time out and throw
-an exception (see C<timeout>, documented elsewhere), in which case no output
-will be consumed so you are free to attempt another C<find_prompt>.
+In scalar context the C<last_response> is returned (see below). In list
+context the gathered response is returned, only split into a list on the
+I<input record separator> (newline).
 
 =item macro($macro_name, ?@params)
 
@@ -170,40 +122,34 @@ commands in the Macro in order, as needed. An exception will be thrown if
 there are insufficient parameters.
 
 An exception will also be thrown if the Match statements in the Macro are not
-successful with the output returned from the device. This is based on the
+successful upon the output returned from the device. This is based on the
 value C<timeout>, which controls how long the module waits for matching
 output.
 
-=item cmd($command_statement)
-
-Execute a single C<send> command statement and consume output until there is a
-match with the current value of C<prompt>. The statement is executed verbatim
-on the device, with a newline appended.
+In scalar context the C<last_response> is returned (see below). In list
+context the gathered response is returned, only split into a list on the
+I<input record separator> (newline).
 
 =item last_response
 
-Returns the gathered output after issueing the most recent C<send> command.
+Returns the gathered output after issueing the last recent C<send> command
+within the most recent C<cmd> or C<prompt>. That is, you get the output from
+the last command sent to the connected device.
 
-=item last_prompt
-
-Returns the Prompt which most recently was matched to terminate gathering of
-output from the connected CLI. This is a simple text string.
-
-=item last_prompt_as_match
-
-Returns the text which was most recently matched to terminate gathering of
-output from the connected CLI, as a regular expression with line start and end
-anchors.
+In scalar context all data is returned. In list context the gathered response
+is returned, only split into a list on the I<input record separator>
+(newline).
 
 =item last_actionset
 
-Returns the complete L<Net::CLI::Interact::ActionSet> that was constructed
-from the most recent C<macro> or C<cmd> execution. This will be a sequence of
-Actions that correspond to C<send> and C<match> statements.
+Returns the complete L<ActionSet|Net::CLI::Interact::ActionSet> that was
+constructed from the most recent C<macro> or C<cmd> execution. This will be a
+sequence of L<Actions|Net::CLI::Interact::Action> that correspond to C<send>
+and C<match> statements.
 
 In the case of a Macro these directly relate to the contents of your
 Phrasebook, with the possible addition of C<match> statements added
-automatically. In the case of a C<cmd> execution, in effect a Macro is
+automatically. In the case of a C<cmd> execution, an "anonymous" Macro is
 constructed which consists of a single C<send> and a single C<match>.
 
 =back
