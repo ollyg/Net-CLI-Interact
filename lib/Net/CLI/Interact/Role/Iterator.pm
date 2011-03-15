61e4c5ef4d5e1edce387cc1852201a579d3df1ee
Oliver Gorwits <oliver@cpan.org>
2011-03-15 21:49:04 +0000

    tidy Iterator docs

--- lib/Net/CLI/Interact/Role/Iterator.pm
+++ lib/Net/CLI/Interact/Role/Iterator.pm
@@ -90,7 +90,7 @@ sub reset { (shift)->_position(-1) }
 
 =head1 DESCRIPTION
 
-This module implements an array-based iterator which may be mixed in to add
+This module implements an array-based iterator which may be mixed-in to add
 management of a sequence of elements and processing of that sequence.
 
 The iterator is loosely based on L<MooseX::Iterator> but is limited to arrays
@@ -110,39 +110,37 @@ C<BUILD> or C<init_arg>. For example:
 
 =head1 METHODS
 
-=over 4
-
-=item count
+=head2 count
 
 The number of elements currently stored in the iterator. Note that this is of
 course not the same as the index of the last item in the iterator (which is
 0-based)
 
-=item first
+=head2 first
 
 Returns the first item in the iterator.
 
-=item last
+=head2 last
 
 Returns the last item in the iterator.
 
-=item item_at($pos)
+=head2 item_at($pos)
 
 Returns the item at the given position in the iterator, or throws an exception
-if C<$pos> is past the end of the iterator. Note that the position is 0-based.
+if C<$pos> is past the end of the iterator. The position is 0-based.
 
-=item insert_at($pos, $iter)
+=head2 insert_at($pos, $iter)
 
-Inserts the contents of the passed iterator starting at (not I<after>) the
-position given. The passed iterator must be a subclass of this module. Note
-that the position is 0-based.
+Inserts the contents of the passed iterator starting I<at> (not I<after>) the
+position given. The passed iterator must also be a consumer of this role. The
+position is 0-based.
 
-=item append($iter)
+=head2 append($iter)
 
-A shorthand for C<insert_at> when you want to add the contents of the passed
-iterator to the end of the sequence.
+Shorthand for C<insert_at> when you want to add the contents of the passed
+iterator I<after> the end of the sequence.
 
-=item idx(?$pos)
+=head2 idx($pos?)
 
 Returns the index (0-based) of the current iterator cursor, or sets the
 cursor if a position (again, 0-based) is passed.
@@ -150,22 +148,22 @@ cursor if a position (again, 0-based) is passed.
 An exception is thrown if you attempt to read the cursor position before
 having read any elements from the iterator, or if the iterator is empty.
 
-=item next
+=head2 next
 
 Returns the next item in the iterator sequence, and advances the cursor.
 Throws an exception if you have already reached the end of the sequence.
 
-=item has_next
+=head2 has_next
 
 Returns true if there are further elements to be read from the iterator.
 
-=item peek
+=head2 peek
 
 Returns the next item in the sequence without advancing the position of the
 cursor. It returns C<undef> if you are already at the end of the sequence.
 
-=item reset
+=head2 reset
 
 Resets the cursor so you can iterate through the sequence of elements again.
 
-=back
+=cut
