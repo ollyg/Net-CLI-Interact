0dcc2c3fd7ccc38133008044725db8daa6a8b080
Oliver Gorwits <oliver@cpan.org>
2011-09-17 23:45:21 +0100

    Newline only added if there was a new *line* in output

--- lib/Net/CLI/Interact/Transport/Base.pm
+++ lib/Net/CLI/Interact/Transport/Base.pm
@@ -135,7 +135,8 @@ sub do_action {
                     (ref $action->value eq ref [] ? (join '|', @{$action->value})
                                                 : $action->value));
                 # put back the partial output and try again
-                $self->stash($self->stash . $maybe_stash . $self->ors);
+                $maybe_stash .= $self->ors if length $maybe_stash;
+                $self->stash($self->stash . $maybe_stash);
                 $self->buffer($last_out);
             }
         }
